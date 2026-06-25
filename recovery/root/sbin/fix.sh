4f90a6af0dfcf58d96475734362efd9840b50fc8
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-06-25 16:30:39 +0900

    Update fix.sh to handle fastboot configuration
    Add fastboot configuration when USB PID is not recognized.

--- recovery/root/sbin/fix.sh
+++ recovery/root/sbin/fix.sh
@@ -4,12 +4,19 @@ pid=$(getprop sys.usb.pid)
 
 if [ "$pid" != "0x4EE0" ] && [ "$pid" != "0x4ee0" ] && [ "$pid" != "4EE0" ] && [ "$pid" != "4ee0" ]; then
     resetprop sys.usb.pid 0x4EE0
+    resetprop sys.usb.config fastboot
     start fastbootd
     exit 0
 fi
 
 if [ -z "$(getprop ro.twrp.fastbootd)" ] || [ "$(getprop ro.twrp.fastbootd)" = "0" ]; then
     resetprop ro.twrp.fastbootd 1
+fi
+
+if [ "$(getprop ro.twrp.fastbootd)" = "1" ] && [ "$(getprop sys.usb.config)" != "fastboot" ]; then
+    resetprop sys.usb.pid 0x4EE0
+    resetprop sys.usb.config fastboot
+    start fastbootd
 fi
 
 exit 0
