c91423390b2f73e3b86487d93d3f6d6109676529
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-06-27 04:43:05 +0900

    Update device.mk

--- device.mk
+++ device.mk
@@ -54,7 +54,6 @@ AB_OTA_POSTINSTALL_CONFIG += \
 # Boot control HAL - Bootctrl
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.2-mtkimpl \
-    android.hardware.boot@1.2-service \
     android.hardware.boot@1.2-mtkimpl.recovery
 
 PRODUCT_PACKAGES_DEBUG += \
