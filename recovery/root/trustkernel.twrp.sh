0fa5e2bc38fbde52cc39ad93be53d0e22e74a19e
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-06-30 01:20:39 +0900

    Remove old TWRP files and copy new ones

--- recovery/root/trustkernel.twrp.sh
+++ recovery/root/trustkernel.twrp.sh
@@ -1,8 +1,5 @@
 #!/sbin/sh
 
-rm -rf /mnt/vendor/persist/t6_twrp/*
-cp -rfp /mnt/vendor/persist/t6/* /mnt/vendor/persist/t6_twrp
-
 rm -rf /mnt/vendor/protect_f/tee_twrp/*
 cp -rfp /mnt/vendor/protect_f/tee/* /mnt/vendor/protect_f/tee_twrp
 setprop twrp.booted 1
