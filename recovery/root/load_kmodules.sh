ab483b11256b4ec456a040f765c8b1d6f9e6c4e4
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-07-04 12:26:56 +0900

    Update load_kmodules.sh

--- recovery/root/load_kmodules.sh
+++ recovery/root/load_kmodules.sh
@@ -6,21 +6,17 @@ load_mod() {
 }
 
 load_mod gz_trusty_mod.ko
-load_mod mtk-mbox.ko
 load_mod mtk-afe-external.ko
 load_mod mtk-pwm.ko
 load_mod i2c-hid.ko
 load_mod focaltech_fp.ko
 load_mod rps_perf.ko
 load_mod gz_ipc_mod.ko
-load_mod mtk_rpmsg_mbox.ko
 load_mod gz_tz_system.ko
 load_mod iommu_gz.ko
 load_mod trusted_mem.ko
 load_mod cmdq-sec-drv.ko
-load_mod mtk_tinysys_ipi.ko
 load_mod mtk_sec_heap.ko
-load_mod scp.ko
 load_mod gz_main_mod.ko
 load_mod mdp_drv_mt6789.ko
 load_mod mtk_u_ether.ko
