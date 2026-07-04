3260b7d93714ecb149a749a5fa4a142564338927
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-07-04 23:52:56 +0900

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -109,6 +109,7 @@ TW_LOAD_VENDOR_DLKM_MODULES := \
     mtk_usb_f_rndis.ko
 
 TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.boot@1.0-impl.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.boot@1.2-mtkimpl.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.boot@1.0.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.boot@1.1.so \
