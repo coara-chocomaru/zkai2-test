b7190b51baa0f2b2a0d61a00d3999d7382232729
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-07-07 04:52:03 +0900

    Update device.mk

--- device.mk
+++ device.mk
@@ -146,13 +146,13 @@ PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
     android.hardware.gatekeeper@1.0-service
 
-# PRODUCT_PACKAGES += \
-#     android.hardware.security.keymint \
-#     android.hardware.security.secureclock \
-#     android.hardware.security.sharedsecret
+PRODUCT_PACKAGES += \
+     android.hardware.security.keymint \
+     android.hardware.security.secureclock \
+     android.hardware.security.sharedsecret
 
-# PRODUCT_PACKAGES += \
-#     android.system.keystore2
+PRODUCT_PACKAGES += \
+     android.system.keystore2
 
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.0.vendor \
